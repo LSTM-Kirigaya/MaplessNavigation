1cfefc775da63591f1c48a8c3eb6ee476da08d52
LSTM-Kirigaya <1193466151@qq.com>
2021-02-12 11:17:15 +0800

    yaml

--- robot/miniBox.py
+++ robot/miniBox.py
@@ -4,28 +4,7 @@ class Robot(object):
     def __init__(self, client : int = 0, basePos : list = [0., 0., 0.], baseOri : list = [0., 0., 0., 1.], useMaxCoor : bool = True,
                  wheel_velocity_max : float = 10., laser_length : float = 10., laser_num : int = 5):
         self.client = client
-        self.wheel_velocity_max = wheel_velocity_max
-        self.laser_length = laser_length
-        self.laser_num = laser_num
-
-        # 常数
-        self.LEFT_WHEEL_JOINT_INDEX = 3
-        self.RIGHT_WHEEL_JOINT_INDEX = 2
-        self.MAX_FORCE = 10.
-        self.TARGET_VELOCITY = 5.
-        self.MULTIPLY = 2.0
-
-        self.DEBUG_TEXT_COLOR = [0., 0., 0.]     # debug文本的颜色
-        self.DEBUG_TEXT_SIZE = 1.2               # debug文本的大小
-        self.MISS_COLOR = [0., 1., 0.]           # 没有命中的激光的颜色
-        self.HIT_COLOR = [1., 0., 0.]            # 命中的激光的颜色
-        self.RAY_DEBUG_LINE_WIDTH = 2.           # 激光的debug线的宽度
-
-        # 机器人参数
-        self.BASE_THICKNESS = 0.2                # 底盘厚度
-        self.BASE_RADIUS = 0.5                   # 底盘半径
-        self.WHEEL_THICKNESS = 0.1               # 轮子厚度
-        self.WHEEL_RADIUS = 0.2                  # 轮子半径
+        
 
         urdf_path = os.path.join(os.path.dirname(__file__), "miniBox")
         
