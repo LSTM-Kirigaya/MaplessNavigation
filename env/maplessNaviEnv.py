4b354c36b468e476e4c5a1e7a701a6862af658fe
Kirigaya <1193466151@qq.com>
2021-02-12 13:29:59 +0800

    参数从YAML中读取

--- env/maplessNaviEnv.py
+++ env/maplessNaviEnv.py
@@ -8,11 +8,17 @@ sys.path.append(path)
 
 import gym
 from gym import spaces
-from robot.test_robot import *
+from robot.utils import *
 
 class maplessNaviEnv(gym.Env):
     metadata = {'render.modes': ['human']}
-    def __init__(self, wheel_velocity_max : float = 10., laser_length : float = 10., laser_num : int = 5):
+    def __init__(self, wheel_velocity_max : float = 10.):
+        # 读入各项参数
+        param_path = os.path.join(os.path.dirname(__file__), "robot_parameters.yaml")
+        param_dict = load(open(param_path, "r", encoding="utf-8"), Loader=Loader)
+        for key, value in param_dict.items():
+            setattr(self, key, value)
+    
         # 动作空间: 左轮速度， 右轮速度
         self.action_space = spaces.Box(
             low=np.array([-wheel_velocity_max, -wheel_velocity_max]),
@@ -21,8 +27,8 @@ class maplessNaviEnv(gym.Env):
         )
         # 状态空间: laser1, ..., 5,   distance, alpha
         self.observation_space = spaces.Box(
-            low=np.array([0.] * laser_num + [0., 0.]),
-            high=np.array([laser_length] * laser_num + [100., np.pi])
+            low=np.array([0.] * self.LASER_NUM + [0., 0.]),
+            high=np.array([self.LASER_LENGTH] * self.LASER_NUM + [self.MAX_DISTANCE, np.pi])
         )
 
         self.np_random, _ = gym.utils.seeding.np_random()
